35299241b2e48277422355889bbf5514c525aff6
aws-training <40118116+aws-training@users.noreply.github.com>
2019-12-16 14:38:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,56 +2,57 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+      	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <?php if (isset($_SESSION['message'])): ?>
-	<div class="msg">
-		<?php 
-			echo $_SESSION['message']; 
-			unset($_SESSION['message']);
-		?>
-	</div>
+        <div class="msg">
+                <?php 
+                      	echo $_SESSION['message']; 
+                        unset($_SESSION['message']);
+                ?>
+        </div>
 <?php endif ?>
-<?php $results = mysqli_query($db, "SELECT * FROM info"); ?>
+<?php $results = $conn->query( "SELECT * FROM info"); ?>
 
 <table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>Address</th>
-			<th colspan="2">Action</th>
-		</tr>
-	</thead>
+       	<thead>
+               	<tr>
+                    	<th>Name</th>
+                        <th>Address</th>
+                        <th colspan="2">Action</th>
+                </tr>
+        </thead>
+        
+       	<?php while ($row = $results->fetch_assoc()) { ?>
+                <tr>
+                    	<td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['address']; ?></td>
+                        <td>
+                            	<a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
+                        </td>
+                        <td>
+                            	<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+                        </td>
+                </tr>
+        <?php } ?>
 	
-	<?php while ($row = mysqli_fetch_array($results)) { ?>
-		<tr>
-			<td><?php echo $row['name']; ?></td>
-			<td><?php echo $row['address']; ?></td>
-			<td>
-				<a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
-			</td>
-			<td>
-				<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
-			</td>
-		</tr>
-	<?php } ?>
-</table>
+	</table>
 
 
-	<form method="post" action="DbOperations.php" >
-		<div class="input-group">
-			<label>Name</label>
-			<input type="text" name="name" value="">
-		</div>
-		<div class="input-group">
-			<label>Address</label>
-			<input type="text" name="address" value="">
-		</div>
-		<div class="input-group">
-			<button class="btn" type="submit" name="save" >Save</button>
-		</div>
-	</form>
+        <form method="post" action="DbOperations.php" >
+                <div class="input-group">
+                        <label>Name</label>
+                        <input type="text" name="name" value="">
+                </div>
+                <div class="input-group">
+                        <label>Address</label>
+                        <input type="text" name="address" value="">
+                </div>
+                <div class="input-group">
+                        <button class="btn" type="submit" name="save" >Save</button>
+                </div>
+        </form>
 </body>
 </html>
